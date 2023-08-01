5b080b5e141727d3b5ccedc89424525b924b35f8
LILITHGAMES\wangyixian <yixianwang@lilith.com>
2023-08-01 18:32:32 +0800

    -init

---
+++ src/components/select/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../select/styles/index.module.less';
+console.log(styles);
+function Select (){
+
+    return (
+        <>
+            <input className={styles['input']}></input>
+        </>
+    )
+}
+export default Select;

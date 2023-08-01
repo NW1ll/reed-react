52c818ab016624eb4fc229d79840e2824a06b55c
LILITHGAMES\wangyixian <yixianwang@lilith.com>
2023-08-01 19:00:44 +0800

    -init1.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
@@ -10,9 +9,38 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "@typescript-eslint/lines-between-class-members": ["error"],
+    "@typescript-eslint/no-dupe-class-members": ["error"],
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-namespace": ["off"],
+    "@typescript-eslint/type-annotation-spacing": [
+      "error",
+      {
+        "before": false,
+        "after": true,
+        "overrides": {
+          "arrow": { "before": true, "after": true }
+        }
+      }
     ],
+    "@typescript-eslint/no-this-alias": [
+      "error",
+      {
+        "allowedNames": ["self"]
+      }
+    ],
+    "react/display-name": 0,
+    "react/prop-types": 0,
+    "semi": 2,
+    "react-hooks/exhaustive-deps": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "block-spacing": [2, "always"],
+    "max-len": [
+      "error",
+      {
+        "code": 140
+      }
+    ],
+    "object-curly-spacing": ["error", "always"]
   },
 }
